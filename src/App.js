747b0aa4b48aa47a61e7f2d1ac368fcdd0f38ce4
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2020-11-23 06:43:22 +0500

    update in App JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Syed Hussnain Ali shah</h1>
+      <p>I am here from Pakistan</p>
+      <ul type="none">
+      <li>Hello</li>
+      <li>Hi </li>
+      <li>How Are you</li>
+      </ul>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Name</th>
+          <th>Roll No#</th>
+          <th>Cell Number</th>
+        </tr>
+        <tr>
+          <th>9-11-2018</th>
+          <td>Hussnain Ali</td>
+          <td>2018-ag-1658</td>
+          <td>03000767150</td>
+        </tr>
+        <tr>
+          <th>10-11-2018</th>
+          <td>Awais Ali</td>
+          <td>2018-ag-1680</td>
+          <td>03328882223</td>
+        </tr>
+      </table>
+
+
+
     </div>
   );
 }
